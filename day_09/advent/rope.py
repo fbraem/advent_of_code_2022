125c9fdfdb2b516b79ad4d6c3653b4efd691bf6d
fbraem <franky.braem@gmail.com>
2022-12-10 12:17:06 +0100

    add __move_head

--- day_09/advent/rope.py
+++ day_09/advent/rope.py
@@ -14,36 +14,36 @@ class Rope:
         return self._knots[-1]
 
     def move(self, direction: str):
-        for index in range(0, len(self._knots) - 1, 2):
-            self.__move_knots(direction, index)
+        self.__move_head(direction)
+        self.__move_knots(direction, 1)
 
-    def __move_knots(self, direction: str, knot: int):
-        next_knot = knot + 1
+    def __move_head(self, direction: str):
         match direction:
             case 'R':  # Right
-                self._knots[knot] = (self._knots[knot][0], self._knots[knot][1] + 1)
+                self._knots[0] = (self._knots[0][0], self._knots[0][1] + 1)
             case 'L':  # Left
-                self._knots[knot] = (self._knots[knot][0], self._knots[knot][1] - 1)
+                self._knots[0] = (self._knots[0][0], self._knots[0][1] - 1)
             case 'U':  # Up
-                self._knots[knot] = (self._knots[knot][0] - 1, self._knots[knot][1])
+                self._knots[0] = (self._knots[0][0] - 1, self._knots[0][1])
             case 'D':  # Down
-                self._knots[knot] = (self._knots[knot][0] + 1, self._knots[knot][1])
+                self._knots[0] = (self._knots[0][0] + 1, self._knots[0][1])
 
+    def __move_knots(self, direction: str, knot: int):
         # Check if the tail must be moved
-        col_distance = self._knots[next_knot][1] - self._knots[knot][1]
-        row_distance = self._knots[next_knot][0] - self._knots[knot][0]
+        col_distance = self._knots[knot][1] - self._knots[knot - 1][1]
+        row_distance = self._knots[knot][0] - self._knots[knot - 1][0]
         print(f'{row_distance=}, {col_distance=}')
 
         if abs(row_distance) > 1 or abs(col_distance) > 1:
             match direction:
                 case 'R':
-                    self._knots[next_knot] = (self._knots[knot][0], self._knots[knot][1] - 1)
+                    self._knots[knot] = (self._knots[knot - 1][0], self._knots[knot - 1][1] - 1)
                 case 'L':
-                    self._knots[next_knot] = (self._knots[knot][0], self._knots[knot][1] + 1)
+                    self._knots[knot] = (self._knots[knot - 1][0], self._knots[knot - 1][1] + 1)
                 case 'U':
-                    self._knots[next_knot] = (self._knots[knot][0] + 1, self._knots[knot][1])
+                    self._knots[knot] = (self._knots[knot - 1][0] + 1, self._knots[knot - 1][1])
                 case 'D':
-                    self._knots[next_knot] = (self._knots[knot][0] - 1, self._knots[knot][1])
+                    self._knots[knot] = (self._knots[knot - 1][0] - 1, self._knots[knot - 1][1])
 
 
     def __str__(self):
