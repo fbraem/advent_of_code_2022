2dac6c3bbbd4197082cbece2071653ee45b70957
fbraem <franky.braem@gmail.com>
2022-12-10 19:10:54 +0100

    part 2

--- day_09/main.py
+++ day_09/main.py
@@ -8,3 +8,11 @@ with open('./files/day9_input') as file:
         grid.move_rope(direction, int(step))
 
 print('visits', grid.number_of_visits)
+
+start_knot = (500, 500)
+grid = Grid(1000, 1000, Rope(*(start_knot,) * 10))
+with open('./files/day9_input') as file:
+    for line in file:
+        direction, step = line.rstrip().split()
+        grid.move_rope(direction, int(step))
+print('visits', grid.number_of_visits)
