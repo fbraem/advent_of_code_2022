5aa7d5a43a6cba301f8c99819c0547f5c73ea5e5
fbraem <franky.braem@gmail.com>
2022-12-10 11:48:43 +0100

    Use knots

--- day_09/advent/rope.py
+++ day_09/advent/rope.py
@@ -2,44 +2,43 @@ from typing import Tuple
 
 
 class Rope:
-    def __init__(self, head: Tuple[int, int], tail: Tuple[int, int]):
-        self._head = head
-        self._tail = tail
+    def __init__(self, *knots: Tuple[int, int]):
+        self._knots = list(knots)
 
     @property
     def head(self) -> Tuple[int, int]:
-        return self._head
+        return self._knots[0]
 
     @property
     def tail(self) -> Tuple[int, int]:
-        return self._tail
+        return self._knots[-1]
 
     def move(self, direction: str):
         match direction:
             case 'R':  # Right
-                self._head = (self._head[0], self._head[1] + 1)
+                self._knots[0] = (self._knots[0][0], self._knots[0][1] + 1)
             case 'L':  # Left
-                self._head = (self._head[0], self._head[1] - 1)
+                self._knots[0] = (self._knots[0][0], self._knots[0][1] - 1)
             case 'U':  # Up
-                self._head = (self._head[0] - 1, self._head[1])
+                self._knots[0] = (self._knots[0][0] - 1, self._knots[0][1])
             case 'D':  # Down
-                self._head = (self._head[0] + 1, self._head[1])
+                self._knots[0] = (self._knots[0][0] + 1, self._knots[0][1])
 
         # Check if the tail must be moved
-        col_distance = self._tail[1] - self._head[1]
-        row_distance = self._tail[0] - self._head[0]
+        col_distance = self._knots[-1][1] - self._knots[0][1]
+        row_distance = self._knots[-1][0] - self._knots[0][0]
         print(f'{row_distance=}, {col_distance=}')
 
         if abs(row_distance) > 1 or abs(col_distance) > 1:
             match direction:
                 case 'R':
-                    self._tail = (self._head[0], self._head[1] - 1)
+                    self._knots[-1] = (self._knots[0][0], self._knots[0][1] - 1)
                 case 'L':
-                    self._tail = (self._head[0], self._head[1] + 1)
+                    self._knots[-1] = (self._knots[0][0], self._knots[0][1] + 1)
                 case 'U':
-                    self._tail = (self._head[0] + 1, self._head[1])
+                    self._knots[-1] = (self._knots[0][0] + 1, self._knots[0][1])
                 case 'D':
-                    self._tail = (self._head[0] - 1, self._head[1])
+                    self._knots[-1] = (self._knots[0][0] - 1, self._knots[0][1])
 
     def __str__(self):
-        return f'{self._head=}, {self._tail=}'
+        return f'{self._knots[0]=}, {self._knots[1]=}'
